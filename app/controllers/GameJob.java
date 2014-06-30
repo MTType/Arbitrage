ae6f65ed6217e582f7dcadd5df4f4d780a856103
Martyn <mtamerlane@yahoo.co.uk>
2014-07-01 00:42:38 +0100

    - Ajax call repeats on page entry instead of button

--- app/controllers/GameJob.java
+++ app/controllers/GameJob.java
@@ -9,7 +9,7 @@ import play.Logger;
 import play.jobs.Every;
 import play.jobs.Job;
 
-@Every("1s")
+@Every("5s")
 public class GameJob extends Job{
     
     private final ExchangeManager exchangeManager = new ExchangeManager();
